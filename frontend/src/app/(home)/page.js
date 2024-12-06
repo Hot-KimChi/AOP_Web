7cf8eb7124ad3ecaf4121f32290e2d58b341eed1
InSeop Hwang <inseop.hwang@hotmail.com>
2024-12-06 21:58:50 +0900

    update

--- frontend/src/app/(home)/page.js
+++ frontend/src/app/(home)/page.js
@@ -2,7 +2,7 @@
 export default function HomePage() {
     return (
       <div className="container text-center py-5">
-        <h1 className="display-4 fw-bold mt-4">AOP Next.js 14 Project!</h1>
+        <h1 className="display-4 fw-bold mt-4">AOP Web Next.js 14 Project!</h1>
       </div>
     );
   }
