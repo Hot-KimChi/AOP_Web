a4c1994d49a9bd07f9bc3fc2bed3106f93383622
InSeop Hwang <inseop.hwang@hotmail.com>
2025-02-08 13:16:37 +0900

    flask(g) 사용, measSetGen 수정정

--- frontend/src/app/(home)/layout.js
+++ frontend/src/app/(home)/layout.js
@@ -1,5 +1,6 @@
 // frontend\src\app\(home)\layout.js
 import Layout from "../../components/Layout";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function HomeLayout({ children }) {
   return (
