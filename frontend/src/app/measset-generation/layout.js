cdd0a367636649ba2e59755c2d0143bc7196ba50
InSeop Hwang <inseop.hwang@hotmail.com>
2025-06-20 21:59:01 +0900

    1) Done - 각 메뉴에 따라서, title 추가함 - DataTable.js삭제(중복이라서 삭제)

--- frontend/src/app/measset-generation/layout.js
+++ frontend/src/app/measset-generation/layout.js
@@ -1,6 +1,10 @@
 // app/measset-generation/layout.js
 import Layout from "../../components/Layout";
 
+export const metadata = {
+  title: "AOP WEB: Measset Generation",
+};
+
 export default function Template({ children }) {
   return (
     <html>
